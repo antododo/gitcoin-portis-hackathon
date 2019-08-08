8ced30433c191fb049e9e23a052195e811defdad
antoine <pl4ym0re@gmail.com>
2019-08-08 12:09:46 +0200

    add compatible tabookey version

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,15 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.24;
 
-contract SimpleStorage {
+import "tabookey-gasless/contracts/RelayRecipient.sol";
+
+contract SimpleStorage is RelayRecipient {
   uint storedData;
+  address private hubAddress;
+
+  constructor() public {
+    hubAddress = 0x1349584869A1C7b8dc8AE0e93D8c15F5BB3B4B87;
+    init_relay_hub(RelayHub(hubAddress));
+  }
 
   function set(uint x) public {
     storedData = x;
@@ -10,4 +18,14 @@ contract SimpleStorage {
   function get() public view returns (uint) {
     return storedData;
   }
+
+    function accept_relayed_call(address relay, address from, bytes memory encoded_function, uint gas_price, uint transaction_fee) public view returns(uint32) {
+        return 0;
+    }
+
+    function post_relayed_call(address relay, address from, bytes memory encoded_function, bool success, uint used_gas, uint transaction_fee) public {
+    }
+
 }
+
+
